c44bd3e092b4e59a64cd1e3030e489b74b6f5dfa
unknown <revchenkodzmitry@gmail.com>
2022-07-04 17:57:41 +0300

    fix: fix webpack.config.js

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from "./loader";
 class AppLoader extends Loader {
     constructor() {
         super("https://newsapi.org/v2/", {
-            apiKey: "", // получите свой ключ https://newsapi.org/
+            apiKey: "6adb1c17f9f74e3dbca69070d986187e",
         });
     }
 }
