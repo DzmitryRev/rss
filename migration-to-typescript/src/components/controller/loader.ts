747fe00489390cf26664039c3d9e50e4b231400a
unknown <revchenkodzmitry@gmail.com>
2022-07-04 19:46:29 +0300

    feat: finish typing loader.ts

--- migration-to-typescript/src/components/controller/loader.ts
+++ migration-to-typescript/src/components/controller/loader.ts
@@ -1,19 +1,53 @@
-class Loader {
-    constructor(baseLink, options) {
+type OptionsType = {
+    apiKey: string;
+};
+
+type GetRespOptionsType = {
+    sources?: string;
+};
+
+type GetRespSettingsType = {
+    endpoint: string;
+    options: GetRespOptionsType;
+};
+
+type CallbackType = (data?: any) => void;
+
+// TODO: data types
+
+interface ILoader {
+    baseLink: string;
+    options: OptionsType;
+    getResp(settings: GetRespSettingsType, callback: CallbackType): void;
+    errorHandler(res: Response): Response;
+    makeUrl(options: GetRespOptionsType, endpoint: string): string;
+    load(
+        method: string,
+        endpoint: string,
+        callback: CallbackType,
+        options: GetRespOptionsType
+    ): void;
+}
+
+class Loader implements ILoader {
+    baseLink: string;
+    options: OptionsType;
+
+    constructor(baseLink: string, options: OptionsType) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
-            console.error('No callback for GET response');
+        { endpoint, options = {} }: GetRespSettingsType,
+        callback: CallbackType = () => {
+            console.error("No callback for GET response");
         }
-    ) {
-        this.load('GET', endpoint, callback, options);
+    ): void {
+        this.load("GET", endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,21 +57,33 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: GetRespOptionsType, endpoint: string): string {
+        const urlOptions: OptionsType & GetRespOptionsType = { ...this.options, ...options };
+
         let url = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+        const entriesArray: [string, string][] = Object.entries(urlOptions).map(
+            (item: [string, string]): [string, string] => {
+                return item;
+            }
+        );
+
+        entriesArray.forEach((item: [string, string]) => {
+            url += `${item[0]}=${item[1]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(
+        method: string,
+        endpoint: string,
+        callback: CallbackType,
+        options: GetRespOptionsType
+    ): void {
         fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
-            .then((res) => res.json())
+            .then(this.errorHandler.bind(this))
+            .then((res: Response) => res.json())
             .then((data) => callback(data))
             .catch((err) => console.error(err));
     }
