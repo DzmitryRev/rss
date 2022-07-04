397b77a8c207fd46ddd62891dfe75124f3677bcf
unknown <revchenkodzmitry@gmail.com>
2022-07-04 17:25:54 +0300

    feat: add eslint

---
+++ migration-to-typescript/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json"],
+    },
+    plugins: ["@typescript-eslint"],
+    rules: { "@typescript-eslint/no-explicit-any": "off" },
+};
--- migration-to-typescript/webpack.config.js
+++ migration-to-typescript/webpack.config.js
@@ -1,43 +1,45 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
+    entry: path.resolve(__dirname, "./src/index.js"),
+    mode: "development",
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
+                loader: "ts-loader",
+                exclude: ["/node_modules/"],
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
     resolve: {
-        extensions: ['.js', '.ts'],
+        extensions: [".js", ".ts"],
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
+        filename: "index.js",
+        path: path.resolve(__dirname, "./dist"),
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
         }),
         new CleanWebpackPlugin(),
     ],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+    const isProductionMode = mode === "prod";
+    const envConfig = isProductionMode
+        ? require("./webpack.prod.config")
+        : require("./webpack.dev.config");
 
     return merge(baseConfig, envConfig);
 };
