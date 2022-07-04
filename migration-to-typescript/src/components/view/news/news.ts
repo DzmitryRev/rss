0128863d750f7fe1189fec327b007a419dafbd3c
unknown <revchenkodzmitry@gmail.com>
2022-07-04 22:36:32 +0300

    feat: finish news.ts typing

--- migration-to-typescript/src/components/view/news/news.ts
+++ migration-to-typescript/src/components/view/news/news.ts
@@ -1,37 +1,52 @@
-import './news.css';
+import { NewsType } from "../../controller/controller";
+import "./news.css";
 
 class News {
-    draw(data) {
-        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+    draw(data: NewsType[]) {
+        const news: NewsType[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
-        const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement | null = document.querySelector("#newsItemTemp");
 
-        news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+        news.forEach((item: NewsType, idx) => {
+            const newsClone = <HTMLTemplateElement | null>newsItemTemp?.content.cloneNode(true);
+            const newsItem = <HTMLDivElement>newsClone?.querySelector(".news__item");
+            const newsMetaPhoto = <HTMLDivElement>newsClone?.querySelector(".news__meta-photo");
+            const newsMetaAuthor = <HTMLLIElement>newsClone?.querySelector(".news__meta-author");
+            const newsMetaDate = <HTMLLIElement>newsClone?.querySelector(".news__meta-date");
+            const newDescriptionTitle = <HTMLHeadingElement>(
+                newsClone?.querySelector(".news__description-title")
+            );
+            const newDescriptionSource = <HTMLHeadingElement>(
+                newsClone?.querySelector(".news__description-source")
+            );
+            const newDescriptionContent = <HTMLParagraphElement>(
+                newsClone?.querySelector(".news__description-content")
+            );
+            const newReadMore = <HTMLParagraphElement>(
+                newsClone?.querySelector(".news__read-more a")
+            );
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            if (idx % 2) newsItem.classList.add("alt");
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
+            newsMetaPhoto.style.backgroundImage = `url(${
+                item.urlToImage || "img/news_placeholder.jpg"
             })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
+            newsMetaAuthor.textContent = item.author || item.source.name;
+            newsMetaDate.textContent = item.publishedAt.slice(0, 10).split("-").reverse().join("-");
 
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+            newDescriptionTitle.textContent = item.title;
+            newDescriptionSource.textContent = item.source.name;
+            newDescriptionContent.textContent = item.description;
+            newReadMore.setAttribute("href", item.url);
 
-            fragment.append(newsClone);
+            fragment.append(newsClone as string | Node);
         });
 
-        document.querySelector('.news').innerHTML = '';
-        document.querySelector('.news').appendChild(fragment);
+        const newsDiv = <HTMLDivElement>document?.querySelector(".news");
+
+        newsDiv.innerHTML = "";
+        newsDiv.appendChild(fragment);
     }
 }
 
