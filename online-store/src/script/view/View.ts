d41e654611056e8f8779f968ebf544889fea92d1
unknown <revchenkodzmitry@gmail.com>
2022-07-12 21:38:37 +0300

    refactor: change product template from Elem to HTML

--- online-store/src/script/view/View.ts
+++ online-store/src/script/view/View.ts
@@ -31,7 +31,6 @@ export class View {
     }
 
     render(products: ProductType[], card: ProductType[]): void {
-        console.log("render");
         this.displayProducts(products, card);
         this.displayCard(card);
     }
@@ -47,75 +46,46 @@ export class View {
         console.log("render products");
         if (this.container) this.container.innerHTML = "";
         products.forEach((product) => {
-            const productContainer = <HTMLDivElement>this.createElement("div", "product");
-            const productImageContainer = <HTMLDivElement>(
-                this.createElement("div", "product__image-container")
-            );
-            const productImage = <HTMLImageElement>this.createElement("img", "product__image");
-            productImage.src = product.image;
-            productImageContainer.insertAdjacentElement("beforeend", productImage);
-            const productDescription = <HTMLDivElement>(
-                this.createElement("div", "product__description")
-            );
-            const productManufacturer = <HTMLSpanElement>(
-                this.createElement("span", "product__manufacturer")
-            );
-            productManufacturer.innerText = product.manufacturer + " ";
-            const productTitle = <HTMLSpanElement>this.createElement("span", "product__title");
-            productTitle.innerText = product.title;
-            const productYearContainer = <HTMLParagraphElement>this.createElement("p");
-            const productYear = <HTMLSpanElement>this.createElement("span", "product__year");
-            productYear.innerText = "Год: " + product.year;
-            productYearContainer.insertAdjacentElement("beforeend", productYear);
-
-            const productColorContainer = <HTMLParagraphElement>this.createElement("p");
-            const productColor = <HTMLSpanElement>this.createElement("span", "product__color");
-            productColor.innerText = "Цвет: " + product.color;
-            productColorContainer.insertAdjacentElement("beforeend", productColor);
-
-            productDescription.insertAdjacentElement("beforeend", productManufacturer);
-            productDescription.insertAdjacentElement("beforeend", productTitle);
-            productDescription.insertAdjacentElement("beforeend", productYearContainer);
-            productDescription.insertAdjacentElement("beforeend", productColorContainer);
-
-            const productFooter = <HTMLDivElement>this.createElement("div", "product__footer");
-            const productPrice = <HTMLSpanElement>this.createElement("span", "product__price");
-            productPrice.innerText = product.price + "$";
-            const buttonAddToCard = <HTMLButtonElement>this.createElement("button", "button");
-            buttonAddToCard.innerText = "В корзину";
-            const buttonRemoveFromCard = <HTMLButtonElement>this.createElement("button", "button");
-            buttonRemoveFromCard.innerText = "Удалить из корзины";
-            buttonRemoveFromCard.classList.add("button-remove-from-card");
-            buttonAddToCard.classList.add("button-add-to-card");
-
-            console.log(card.find((item) => item.id === product.id));
-            if (card.find((item) => item.id === product.id)) {
-                buttonAddToCard.classList.add("display-none");
-                productPrice.classList.add("display-none");
-            } else {
-                buttonRemoveFromCard.classList.add("display-none");
-                productPrice.classList.remove("display-none");
-            }
-            buttonAddToCard.setAttribute("product-id", (product.id as unknown) as string);
-            buttonRemoveFromCard.setAttribute("product-id", (product.id as unknown) as string);
-            // this.buttonAddToCard?.push(button);
-
-            productFooter.insertAdjacentElement("beforeend", productPrice);
-            productFooter.insertAdjacentElement("beforeend", buttonAddToCard);
-            productFooter.insertAdjacentElement("beforeend", buttonRemoveFromCard);
-
-            productContainer.insertAdjacentElement("beforeend", productImageContainer);
-            productContainer.insertAdjacentElement("beforeend", productDescription);
-            productContainer.insertAdjacentElement("beforeend", productFooter);
-
-            this.container?.insertAdjacentElement("beforeend", productContainer);
+            const isInCard = card.find((item) => item.id === product.id);
+            const template = `
+                <div class="product">
+                    <div class="product__image-container">
+                        <img
+                            class="product__image"
+                            src="${product.image}"
+                            alt="${product.title}"
+                        />
+                    </div>
+                    <div class="product__description">
+                        <span class="product__manufacturer">${product.manufacturer}</span>
+                        <span class="product__title">${product.title}</span>
+                        <p>Год: <span class="product__year">${product.year}</span></p>
+                        <p>Цвет: <span class="product__color">${product.color}</span></p>
+                    </div>
+                    <div class="product__footer">
+                        <span class="product__price ${isInCard ? "display-none" : ""}">
+                        ${product.price}$</span>
+                        <button id="test2" 
+                                class="button button-add-to-card ${isInCard ? "display-none" : ""}"
+                                product-id='${product.id}'
+                                >В корзину</button>
+                        <button id="test2" 
+                                class="button button-remove-from-card ${isInCard ? "" : "display-none"}"
+                                product-id='${product.id}'
+                                >В корзину</button>
+                    </div>
+                </div>
+            `;
+            this.container?.insertAdjacentHTML("beforeend", template);
         });
     }
 
     renderProductFooter(productId: string) {
+        console.log("Eee");
         const buttonAddToCard = <HTMLButtonElement>(
             document.querySelector(`.button-add-to-card[product-id='${productId}']`)
         );
+        console.log(buttonAddToCard);
         const buttonRemoveFromCard = <HTMLButtonElement>(
             document.querySelector(`.button-remove-from-card[product-id='${productId}']`)
         );
@@ -150,3 +120,70 @@ export class View {
         });
     }
 }
+
+
+
+
+
+// const productContainer = <HTMLDivElement>this.createElement("div", "product");
+            // const productImageContainer = <HTMLDivElement>(
+            //     this.createElement("div", "product__image-container")
+            // );
+            // const productImage = <HTMLImageElement>this.createElement("img", "product__image");
+            // productImage.src = product.image;
+            // productImageContainer.insertAdjacentElement("beforeend", productImage);
+            // const productDescription = <HTMLDivElement>(
+            //     this.createElement("div", "product__description")
+            // );
+            // const productManufacturer = <HTMLSpanElement>(
+            //     this.createElement("span", "product__manufacturer")
+            // );
+            // productManufacturer.innerText = product.manufacturer + " ";
+            // const productTitle = <HTMLSpanElement>this.createElement("span", "product__title");
+            // productTitle.innerText = product.title;
+            // const productYearContainer = <HTMLParagraphElement>this.createElement("p");
+            // const productYear = <HTMLSpanElement>this.createElement("span", "product__year");
+            // productYear.innerText = "Год: " + product.year;
+            // productYearContainer.insertAdjacentElement("beforeend", productYear);
+
+            // const productColorContainer = <HTMLParagraphElement>this.createElement("p");
+            // const productColor = <HTMLSpanElement>this.createElement("span", "product__color");
+            // productColor.innerText = "Цвет: " + product.color;
+            // productColorContainer.insertAdjacentElement("beforeend", productColor);
+
+            // productDescription.insertAdjacentElement("beforeend", productManufacturer);
+            // productDescription.insertAdjacentElement("beforeend", productTitle);
+            // productDescription.insertAdjacentElement("beforeend", productYearContainer);
+            // productDescription.insertAdjacentElement("beforeend", productColorContainer);
+
+            // const productFooter = <HTMLDivElement>this.createElement("div", "product__footer");
+            // const productPrice = <HTMLSpanElement>this.createElement("span", "product__price");
+            // productPrice.innerText = product.price + "$";
+            // const buttonAddToCard = <HTMLButtonElement>this.createElement("button", "button");
+            // buttonAddToCard.innerText = "В корзину";
+            // const buttonRemoveFromCard = <HTMLButtonElement>this.createElement("button", "button");
+            // buttonRemoveFromCard.innerText = "Удалить из корзины";
+            // buttonRemoveFromCard.classList.add("button-remove-from-card");
+            // buttonAddToCard.classList.add("button-add-to-card");
+
+            // console.log(card.find((item) => item.id === product.id));
+            // if (card.find((item) => item.id === product.id)) {
+            //     buttonAddToCard.classList.add("display-none");
+            //     productPrice.classList.add("display-none");
+            // } else {
+            //     buttonRemoveFromCard.classList.add("display-none");
+            //     productPrice.classList.remove("display-none");
+            // }
+            // buttonAddToCard.setAttribute("product-id", (product.id as unknown) as string);
+            // buttonRemoveFromCard.setAttribute("product-id", (product.id as unknown) as string);
+            // // this.buttonAddToCard?.push(button);
+
+            // productFooter.insertAdjacentElement("beforeend", productPrice);
+            // productFooter.insertAdjacentElement("beforeend", buttonAddToCard);
+            // productFooter.insertAdjacentElement("beforeend", buttonRemoveFromCard);
+
+            // productContainer.insertAdjacentElement("beforeend", productImageContainer);
+            // productContainer.insertAdjacentElement("beforeend", productDescription);
+            // productContainer.insertAdjacentElement("beforeend", productFooter);
+
+            // this.container?.insertAdjacentElement("beforeend", productContainer);
