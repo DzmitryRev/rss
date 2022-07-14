ac11805583c8e287741db7b161befb95a788981e
unknown <revchenkodzmitry@gmail.com>
2022-07-14 23:43:09 +0300

    refactor: cleanup Controller

--- online-store/src/script/controller/Controller.ts
+++ online-store/src/script/controller/Controller.ts
@@ -1,4 +1,3 @@
-import { ProductType } from "../../data/products";
 import { IModel } from "../model/Model";
 import { IView } from "../view/View";
 
@@ -18,7 +17,6 @@ export class Controller implements IController {
         this.model = model;
         this.view = view;
     }
-    // finish
     start() {
         // set up events
         this.view.addToCardEvent((id: string) => {
@@ -27,58 +25,18 @@ export class Controller implements IController {
         this.view.removeFromCardEvent((id: string) => {
             this.handleRemoveFromCard(id);
         });
-        // this.view.colorFilterEvent((method: "ADD" | "DELETE", value: string) => {
-        //     this.handleAddFilter(value);
-        // });
-        // this.view.yearFilterEvent((method: "ADD" | "DELETE", value: string) => {
-        //     this.handleYearFilter(method, value);
-        // });
-        // init card local storage
-        // this.model.refreshCard();
-        // init render
         this.render();
     }
-    // render All
-    // finish
+
+    // render all
+
     render() {
-        // this.view.settingsBlock.render(this.model._products);
-        // this.view.productsBlock.render(
-        //     this.model.getProducts((products) => {
-        //         console.log("Aaa");
-        //     })
-        // );
         this._getProducts();
-        // this._getProducts();
-        // this.view.renderCard(this.model.getCardStorage());
-        // this.view.renderProductFooter(this.model._products, this.model.getCardStorage());
         this._getCard();
     }
-    // // finish
-    // hanlerAddToCard(id: string) {
-    //     this.model.addToCard(id, (card: ProductType[], productId: string) => {
-    //         this.view.renderCard(card);
-    //         this.view.renderProductFooter(productId);
-    //     });
-    // }
-    // // finish
-    // hanlerRemoveFromCard(id: string) {
-    //     this.model.removeFromCard(id, (card: ProductType[], productId: string) => {
-    //         this.view.renderCard(card);
-    //         this.view.renderProductFooter(productId);
-    //     });
-    // }
-    // handleColorFilter(method: "ADD" | "DELETE", value: string) {
-    //     this.model.toggleColor(method, value, (products) => {
-    //         this.view.productsBlock.render(products);
-    //     });
-    // }
-    // handleYearFilter(method: "ADD" | "DELETE", value: string) {
-    //     this.model.toggleYear(method, value, (products) => {
-    //         this.view.productsBlock.render(products);
-    //     });
-    // }
 
-    // CARD
+    // card handlers
+
     handleAddToCard(id: string) {
         this.model.addToCard(id, () => {
             this._getCard();
@@ -114,12 +72,3 @@ export class Controller implements IController {
         });
     }
 }
-
-// TODO:
-// 1. Стилизовать CardCount // +
-// 1.1 Не больше 20 товаров в корзине!
-// 2. Добавить логику открытия бокового меню
-// 3. Сортировка
-// 4. Фильтрация
-// 5. Поиск
-// 6. Корзина???
