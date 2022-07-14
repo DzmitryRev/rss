89c28720df57e38da68d693e91289d76492cde62
unknown <revchenkodzmitry@gmail.com>
2022-07-14 23:40:54 +0300

    refactor: cleanup Model

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -6,6 +6,7 @@ import "./assets/card-icon.svg";
 import { Model } from "./script/model/Model";
 import { Controller, IController } from "./script/controller/Controller";
 import { View } from "./script/view/View";
+import { products } from "./data/products";
 
 const test = document.querySelector("#test");
 const el = document.querySelector("#el");
@@ -14,5 +15,5 @@ test?.addEventListener("click", () => {
     el?.classList.toggle("open");
 });
 
-const app: IController = new Controller(new Model(), new View());
+const app: IController = new Controller(new Model(products), new View());
 app.start();
--- online-store/src/script/model/Model.ts
+++ online-store/src/script/model/Model.ts
@@ -1,70 +1,42 @@
-import { products, ProductType } from "../../data/products";
-import { CheckboxFilter } from "../utils/CheckboxFilter";
+import { ProductType } from "../../data/products";
+
+type FiltersType = { color: string[]; year: string[] };
 
 export interface IModel {
     _products: ProductType[];
-    // card: ProductType[];
-    // filter(): ProductType[];
     findProduct(id: string): void;
-    // refreshCard(): void;
-    // addToCard(productId: string, callback: (card: ProductType[], productId: string) => void): void;
-    // removeFromCard(
-    //     productId: string,
-    //     callback: (card: ProductType[], productId: string) => void
-    // ): void;
-    // toggleColor(
-    //     method: "ADD" | "DELETE",
-    //     value: string,
-    //     callback: (products: ProductType[], card: ProductType[]) => void
-    // ): void;
-    // toggleYear(
-    //     method: "ADD" | "DELETE",
-    //     value: string,
-    //     callback: (products: ProductType[], card: ProductType[]) => void
-    // ): void;
-    // yearFilter: CheckboxFilter;
 
-    // Refactor
+    // storage listeners
+    getCardStorage(): ProductType[];
+    getFiltersStorage(): FiltersType;
+    setCardStorage(card: ProductType[]): void;
+    setFiltersStorage: (filters: FiltersType) => void;
+
+    // connectors for data
+    getProducts(callback: (products: ProductType[], currentCard: ProductType[]) => void): void;
     getCard(callback: (products: ProductType[], currentCard: ProductType[]) => void): void;
+
+    // manage card
     addToCard(id: string, callback: () => void): void;
     removeFromCard(id: string, callback: () => void): void;
 
-    getCardStorage(): ProductType[];
-    setCardStorage(card: ProductType[]): void;
-    getProducts(callback: (products: ProductType[], currentCard: ProductType[]) => void): void;
+    // manage filtre
     addFilter(field: string, value: string, callback: () => void): void;
     removeFilter(field: string, value: string, callback: () => void): void;
 }
 
 export class Model implements IModel {
     _products: ProductType[];
-    // card: ProductType[];
-    // colorFilter: CheckboxFilter;
-    // yearFilter: CheckboxFilter;
-    // Refactor
 
     getCardStorage: () => ProductType[];
     setCardStorage: (card: ProductType[]) => void;
 
-    getFiltersStorage: () => {
-        color: string[];
-        year: string[];
-    };
-    setFiltersStorage: (filters: { color: string[]; year: string[] }) => void;
+    getFiltersStorage: () => FiltersType;
+    setFiltersStorage: (filters: FiltersType) => void;
 
-    filters: {
-        color: string[];
-        year: string[];
-    };
-    //
-
-    constructor() {
+    constructor(products: ProductType[]) {
         this._products = products;
-        // this.card = [];
-        // this.colorFilter = new CheckboxFilter();
-        // this.yearFilter = new CheckboxFilter();
 
-        // Refactor
         const localStorage = window.localStorage;
 
         this.getCardStorage = () => {
@@ -78,42 +50,32 @@ export class Model implements IModel {
             localStorage.setItem("revchenko-store-card", JSON.stringify(card));
         };
 
-        this.filters = {
+        const defaultFilters: FiltersType = {
             color: [],
             year: [],
         };
 
         this.getFiltersStorage = () => {
             return (
-                <
-                    {
-                        color: string[];
-                        year: string[];
-                    }
-                >JSON.parse(<string>localStorage.getItem("revchenko-store-filters")) || {
-                    color: [],
-                    year: [],
-                }
+                <FiltersType>JSON.parse(<string>localStorage.getItem("revchenko-store-filters")) ||
+                defaultFilters
             );
         };
         this.setFiltersStorage = (filters: { color: string[]; year: string[] }) => {
             localStorage.setItem("revchenko-store-filters", JSON.stringify(filters));
         };
-
-        //
     }
 
-    // Refactor
+    findProduct(id: string): ProductType | undefined {
+        return this._products.find((item) => item.id === id);
+    }
+
+    // Card logic
+
     addToCard(id: string, callback: () => void): void {
         const card = this.getCardStorage();
         const product = this.findProduct(id);
         if (!product || card.find((productInCard) => productInCard.id === id)) return;
-        // if (!card.length) {
-        //     const newCard: ProductType[] = [product];
-        //     this.setCardStorage(newCard);
-        //     callback();
-        //     return;
-        // }
         const newCard: ProductType[] = [...card, product];
         this.setCardStorage(newCard);
         callback();
@@ -130,22 +92,8 @@ export class Model implements IModel {
         const card = this.getCardStorage();
         callback(this._products, card);
     }
-    //
 
-    // finish
-    findProduct(id: string): ProductType | undefined {
-        return this._products.find((item) => item.id === id);
-    }
-
-    // filter(): ProductType[] {
-    //     const filtredProducts = this._products.filter((item) => {
-    //         return (
-    //             (!this.colorFilter.values.length || this.colorFilter.values.includes(item.color)) &&
-    //             (!this.yearFilter.values.length || this.yearFilter.values.includes(item.year))
-    //         );
-    //     });
-    //     return filtredProducts;
-    // }
+    // Filter logic
 
     addFilter(field: string, value: string, callback: () => void) {
         const filters = this.getFiltersStorage();
@@ -175,49 +123,4 @@ export class Model implements IModel {
         const currentCard = this.getCardStorage();
         callback(filtredPropucts, currentCard);
     }
-
-    // toggleColor(
-    //     method: "ADD" | "DELETE",
-    //     value: string,
-    //     callback: (products: ProductType[], card: ProductType[]) => void
-    // ) {
-    //     this.colorFilter.toggleValue(method, value);
-    //     console.log(this.colorFilter.values);
-    //     callback(this.filter(), this.card);
-    // }
-    // toggleYear(
-    //     method: "ADD" | "DELETE",
-    //     value: string,
-    //     callback: (products: ProductType[], card: ProductType[]) => void
-    // ) {
-    //     this.yearFilter.toggleValue(method, value);
-    //     callback(this.filter(), this.card);
-    // }
-
-    // // finish
-    // refreshCard(): void {
-    //     this.card =
-    //         <ProductType[]>JSON.parse(<string>localStorage.getItem("revchenko-store-card")) || [];
-    // }
-    // // finish
-    // addToCard(productId: string, callback: (card: ProductType[], productId: string) => void): void {
-    //     if (this.card.length >= 20) {
-    //         alert("Не больше 20 товаров!");
-    //         return;
-    //     }
-    //     const product: ProductType | undefined = this.findProduct(productId);
-    //     const newCard: ProductType[] = [...this.card];
-    //     if (newCard.filter((item) => item.id === productId).length > 0 || !product) return;
-    //     newCard.push(product);
-    //     localStorage.setItem("revchenko-store-card", JSON.stringify(newCard));
-    //     this.refreshCard();
-    //     callback(this.card, productId);
-    // }
-    // // finish
-    // removeFromCard(productId: string, callback: (card: ProductType[], productId: string) => void) {
-    //     const newCard: ProductType[] = this.card.filter((item) => item.id !== productId);
-    //     localStorage.setItem("revchenko-store-card", JSON.stringify(newCard));
-    //     this.refreshCard();
-    //     callback(this.card, productId);
-    // }
 }
