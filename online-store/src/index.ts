9ae63b2626af4df44dc6f1bd5b721146e41dc6db
unknown <revchenkodzmitry@gmail.com>
2022-07-18 13:43:28 +0300

    feat: install nouislider

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,20 +2,48 @@
 import "./scss/index.scss";
 // images
 import "./assets/card-icon.svg";
-import "./assets/product-1.jpg"
+import "./assets/product-1.jpg";
 //
 import { Model } from "./script/model/Model";
 import { Controller, IController } from "./script/controller/Controller";
 import { View } from "./script/view/View";
 import { products } from "./data/products";
+import noUiSlider, { target } from "../node_modules/nouislider/dist/nouislider";
+
+// import
+
+// const range = <HTMLInputElement>document.querySelector("#range");
+
+// let a: string = multirange();
+// document.addEventListener("DOMContentLoaded", () => {
+
+// });
+
+const slider = <target>document.getElementById("slider");
+const btn = <HTMLButtonElement>document.getElementById("test");
+const a = noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        min: 0,
+        max: 100,
+    },
+});
+
+btn.addEventListener("click", () => {
+    // a.destroy(); // delete range
+    // a.reset(); // reset to default
+    // console.log(a.get()); // get values [string, string]
+    // a.set([0, 100]);
+});
 
 const app: IController = new Controller(new Model(products), new View());
 app.start();
 
 // TODO:
-// 1. Add reset filters
+// 1. Add reset filters -- finish
 // 1.1 IMAGES
-// 2. Add search
+// 2. Add search -- finish
 // 3. Add range filters
 // 4. Add close btn for sidebar
 // 5. Convert Template to class and insert them into View
