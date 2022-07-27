7bce9e2a46778f69e40ea6df3e379ec90537890b
unknown <revchenkodzmitry@gmail.com>
2022-07-27 09:38:32 +0300

    refactor: finish refact news.ts

--- migration-to-typescript/src/components/view/news/news.ts
+++ migration-to-typescript/src/components/view/news/news.ts
@@ -1,52 +1,65 @@
-import { NewsType } from "../../controller/controller";
+import { NewsType } from "../../../types/index";
 import "./news.css";
 
+type Element<T> = T | null;
+
 class News  {
-    draw(data: NewsType[]) {
+    draw(data: NewsType[]): void {
         const news: NewsType[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment: DocumentFragment = document.createDocumentFragment();
-        const newsItemTemp: HTMLTemplateElement | null = document.querySelector("#newsItemTemp");
+        const newsItemTemp: Element<HTMLTemplateElement> = document.querySelector("#newsItemTemp");
 
         news.forEach((item: NewsType, idx) => {
-            const newsClone = <HTMLTemplateElement | null>newsItemTemp?.content.cloneNode(true);
-            const newsItem = <HTMLDivElement>newsClone?.querySelector(".news__item");
-            const newsMetaPhoto = <HTMLDivElement>newsClone?.querySelector(".news__meta-photo");
-            const newsMetaAuthor = <HTMLLIElement>newsClone?.querySelector(".news__meta-author");
-            const newsMetaDate = <HTMLLIElement>newsClone?.querySelector(".news__meta-date");
-            const newDescriptionTitle = <HTMLHeadingElement>(
+            const newsClone = <Element<HTMLTemplateElement>>newsItemTemp?.content.cloneNode(true);
+            const newsItem = <Element<HTMLDivElement>>newsClone?.querySelector(".news__item");
+            const newsMetaPhoto = <Element<HTMLDivElement>>newsClone?.querySelector(".news__meta-photo");
+            const newsMetaAuthor = <Element<HTMLLIElement>>newsClone?.querySelector(".news__meta-author");
+            const newsMetaDate = <Element<HTMLLIElement>>newsClone?.querySelector(".news__meta-date");
+            const newDescriptionTitle = <Element<HTMLHeadingElement>>(
                 newsClone?.querySelector(".news__description-title")
             );
-            const newDescriptionSource = <HTMLHeadingElement>(
+            const newDescriptionSource = <Element<HTMLHeadingElement>>(
                 newsClone?.querySelector(".news__description-source")
             );
-            const newDescriptionContent = <HTMLParagraphElement>(
+            const newDescriptionContent = <Element<HTMLParagraphElement>>(
                 newsClone?.querySelector(".news__description-content")
             );
-            const newReadMore = <HTMLParagraphElement>(
+            const newReadMore = <Element<HTMLParagraphElement>>(
                 newsClone?.querySelector(".news__read-more a")
             );
 
-            if (idx % 2) newsItem.classList.add("alt");
-
-            newsMetaPhoto.style.backgroundImage = `url(${
+            if (idx % 2) newsItem?.classList.add("alt");
+            if(newsMetaPhoto) {
+                newsMetaPhoto.style.backgroundImage = `url(${
                 item.urlToImage || "img/news_placeholder.jpg"
-            })`;
-            newsMetaAuthor.textContent = item.author || item.source.name;
-            newsMetaDate.textContent = item.publishedAt.slice(0, 10).split("-").reverse().join("-");
-
-            newDescriptionTitle.textContent = item.title;
-            newDescriptionSource.textContent = item.source.name;
-            newDescriptionContent.textContent = item.description;
-            newReadMore.setAttribute("href", item.url);
-
+            })`;}
+            if(newsMetaAuthor) {
+                newsMetaAuthor.textContent = item.author || item.source.name;
+            }
+            if(newsMetaDate) {
+                newsMetaDate.textContent = item.publishedAt.slice(0, 10).split("-").reverse().join("-");
+            }
+            if(newDescriptionTitle) {
+                newDescriptionTitle.textContent = item.title;
+            }
+            if(newDescriptionSource) { 
+                newDescriptionSource.textContent = item.source.name;
+            }
+           if(newDescriptionContent){
+                newDescriptionContent.textContent = item.description;
+            }
+            if(newReadMore) {
+                newReadMore.setAttribute("href", item.url);
+            }
             fragment.append(newsClone as string | Node);
         });
 
-        const newsDiv = <HTMLDivElement>document?.querySelector(".news");
-
-        newsDiv.innerHTML = "";
-        newsDiv.appendChild(fragment);
+        const newsDiv = <Element<HTMLDivElement>>document?.querySelector(".news");
+        if(newsDiv) {
+            newsDiv.innerHTML = "";
+            newsDiv.appendChild(fragment);
+        }
     }
 }
 
