9756da1587e1c79b731a62af110e96499a770e32
unknown <revchenkodzmitry@gmail.com>
2022-07-31 16:16:43 +0300

    feat: create API class

--- async-race/src/scripts/API/Api.ts
+++ async-race/src/scripts/API/Api.ts
@@ -1,6 +1,11 @@
-// export function getCars() {
+/* eslint-disable no-unused-vars */
+// export function createCar(car: { name: string; color: string }) {
 //   fetch('http://localhost:3000/garage', {
-//     method: 'GET',
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(car),
 //   })
 //     .then((res) => res.json())
 //     .then((res) => {
@@ -8,27 +13,42 @@
 //     });
 // }
 
-export function getCar(id: number) {
-  fetch(`http://localhost:3000/garage/${id}`, {
-    method: 'GET',
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-    });
-}
-export function createCar(car: { name: string; color: string }) {
-  fetch('http://localhost:3000/garage', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(car),
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-    });
+// export function deleteCar(id: number) {}
+
+type CarType = {
+  name: string;
+  color: string;
+  id: number;
+};
+
+class API {
+  host: string;
+
+  constructor() {
+    this.host = 'http://localhost:3000/';
+  }
+
+  async getCars(callback: (cars: CarType[]) => void) {
+    await fetch(`${this.host}garage`)
+      .then((res) => res.json())
+      .then((res: CarType[]) => {
+        callback(res);
+      })
+      .catch(() => {
+        throw new Error();
+      });
+  }
+
+  async getCar(id: number, callback: (car: CarType) => void) {
+    await fetch(`${this.host}garage/${id}`)
+      .then((res) => res.json())
+      .then((res: CarType) => {
+        callback(res);
+      })
+      .catch(() => {
+        throw new Error();
+      });
+  }
 }
 
-// export function deleteCar(id: number) {}
+export default API;
