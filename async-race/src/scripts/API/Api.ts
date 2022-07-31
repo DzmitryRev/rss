b104903574a2190d245562f8e48db5b8d110115c
unknown <revchenkodzmitry@gmail.com>
2022-07-31 16:37:50 +0300

    feat: add methods to fetch data

--- async-race/src/scripts/API/Api.ts
+++ async-race/src/scripts/API/Api.ts
@@ -20,6 +20,10 @@ type CarType = {
   color: string;
   id: number;
 };
+type EngineSettingsType = {
+  velocity: number;
+  distance: number;
+};
 
 class API {
   host: string;
@@ -43,6 +47,57 @@ class API {
     await fetch(`${this.host}garage/${id}`)
       .then((res) => res.json())
       .then((res: CarType) => {
+        callback(res);
+      })
+      .catch(() => {
+        throw new Error();
+      });
+  }
+
+  async createCar(car: Omit<CarType, 'id'>, callback: () => void) {
+    await fetch(`${this.host}garage/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(car),
+    })
+      .then(() => callback())
+      .catch(() => {
+        throw new Error();
+      });
+  }
+
+  async removeCar(id: number, callback: () => void) {
+    await fetch(`${this.host}garage/${id}`, {
+      method: 'DELETE',
+    })
+      .then(() => callback())
+      .catch(() => {
+        throw new Error();
+      });
+  }
+
+  async updateCar(id: number, car: Omit<CarType, 'id'>, callback: () => void) {
+    await fetch(`${this.host}garage/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(car),
+    })
+      .then(() => callback())
+      .catch(() => {
+        throw new Error();
+      });
+  }
+
+  async startEngine(id: number, callback: (engineSettings: EngineSettingsType) => void) {
+    await fetch(`${this.host}engine/${id}/?status='started'`, {
+      method: 'PATCH',
+    })
+      .then((res) => res.json())
+      .then((res: EngineSettingsType) => {
         callback(res);
       })
       .catch(() => {
