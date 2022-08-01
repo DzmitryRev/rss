6e82a2ceb0e0f4a8c07678c063d62775093f5884
unknown <revchenkodzmitry@gmail.com>
2022-08-01 21:38:17 +0300

    feat: create Winnerts

--- async-race/src/scripts/views/Winners.ts
+++ async-race/src/scripts/views/Winners.ts
@@ -1,14 +1,14 @@
 import Component from '../../core/component/Component';
 import VirtualNode from '../../core/virtual-node/VirtualNode';
 
-class Garage extends Component {
+class Winners extends Component {
   //   constructor() {}
 
   render() {
-    const element = new VirtualNode('div', '', ['GARAGE']);
+    const element = new VirtualNode('div', '', ['WINNERS']);
     if (!this.element) this.element = element;
     return element;
   }
 }
 
-export default Garage;
+export default Winners;
