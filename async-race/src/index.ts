90bf8e51abb438621ac3acb8c693cb06831f269b
unknown <revchenkodzmitry@gmail.com>
2022-08-01 19:26:45 +0300

    feat: add VirtualNode class

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,15 +1,97 @@
-import API from './scripts/API/Api';
-import { CarType } from './scripts/API/types';
-import GarageModel from './scripts/models/GarageModel';
+// /* eslint-disable max-classes-per-file */
+// import { CarType } from './scripts/API/types';
+// import GarageModel from './scripts/models/GarageModel';
+// import Root from './scripts/core/Root';
 
-const a = new GarageModel();
+// const a = new GarageModel();
 
-a.getCars((cars: CarType[]) => {
-  console.log(cars);
-});
+// class Car {
+//   el: Root;
 
-a.page = 2;
-console.log(a.page);
-a.getCars((cars: CarType[]) => {
-  console.log(cars);
-});
+//   state: {
+//     name: string;
+//     color: string;
+//   };
+
+//   props: {
+//     name: string;
+//     color: string;
+//   };
+
+//   constructor(props: { name: string; color: string }) {
+//     this.props = props;
+//     this.state = {
+//       name: props.name,
+//       color: props.color,
+//     };
+//   }
+
+//   render() {
+//     console.log('render car');
+//     const el = new Root('div', [new Root('span',
+// [`${this.props.name}`, `${this.props.color}`])]);
+//     if (!this.el) this.el = el;
+//     return el;
+//   }
+// }
+
+// interface Component<T> {
+//   el: Root;
+//   render: () => Root;
+//   state?: T;
+// }
+
+// class App {
+//   el: Root;
+
+//   model: GarageModel;
+
+//   state: {
+//     cars: CarType[];
+//   };
+
+//   constructor() {
+//     this.state = {
+//       cars: [],
+//     };
+//     this.model = a;
+//     // this.fetchCars();
+//     this.fetchCars();
+//   }
+
+//   setState(newState: { cars: CarType[] }) {
+//     this.state = newState;
+//     const newEl = this.render();
+//     this.el.children = newEl.children;
+//     this.el.update();
+//   }
+
+//   //   update() {
+//   //     const newEl = this.render();
+//   //     this.el.children = newEl.children;
+//   //     this.el.update();
+//   //   }
+
+//   fetchCars() {
+//     console.log(this.model);
+//     this.model.getCars((cars) => {
+//       this.setState({
+//         cars,
+//       });
+//       console.log(cars);
+//     });
+//   }
+
+//   render() {
+//     console.log('render App');
+//     const el = new Root(
+//       'div',
+//       ['Hello',
+// ...this.state.cars.map((car) => new Car({ name: car.name, color: car.color }).render())],
+//     );
+//     if (!this.el) this.el = el;
+//     return el;
+//   }
+// }
+
+// document.getElementById('app').insertAdjacentElement('beforeend', new App().render().render());
