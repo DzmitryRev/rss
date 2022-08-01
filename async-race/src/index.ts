7f5a7cf69db8e36cdecbc3d3ad19eb1745e4ad01
unknown <revchenkodzmitry@gmail.com>
2022-08-01 22:08:45 +0300

    feat: create Router (without location )

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,64 +1,56 @@
-/* eslint-disable class-methods-use-this */
-/* eslint-disable import/extensions */
-
-import VirtualNode from './core/virtual-node/VirtualNode';
 import renderDOM from './core/render/renderDOM';
-import Component from './core/component/Component';
-import API from './scripts/API/Api';
-import CarE from './car';
 
-import { IState } from './core/component/types';
-import { CarType } from './scripts/API/types';
+import App from './scripts/App';
 
-interface IAppState extends IState {
-  count: number;
-  cars: CarType[];
-}
-class App extends Component {
-  state: IAppState;
+// interface IAppState extends IState {
+//   count: number;
+//   cars: CarType[];
+// }
+// class App extends Component {
+//   state: IAppState;
 
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      cars: [],
-    };
-  }
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0,
+//       cars: [],
+//     };
+//   }
 
-  fetchCars() {
-    API.getCars(1, (cars) => {
-      this.setState({ ...this.state, cars });
-    });
-  }
+//   fetchCars() {
+//     API.getCars(1, (cars) => {
+//       this.setState({ ...this.state, cars });
+//     });
+//   }
 
-  onMount(): void {
-    this.fetchCars();
-  }
+//   onMount(): void {
+//     this.fetchCars();
+//   }
 
-  render(): VirtualNode {
-    console.log('render App');
-    const element = new VirtualNode('div', '', [
-      'hello',
-      new VirtualNode('h1', '', ['hello']),
-      new VirtualNode('h4', '', ['world']),
-      new VirtualNode('div', '', [
-        new VirtualNode('span', '', [`${this.state.count}`], {
-          type: 'click',
-          callback: () => {
-            this.setState<IAppState>({
-              ...this.state,
-              count: this.state.count + 1,
-            });
-          },
-        }),
-        new VirtualNode('span', '', ['world']),
-        ...this.state.cars.map((car) => new CarE({ name: car.name, color: car.color }).render()),
-      ]),
-    ]);
-    if (!this.element) this.element = element;
-    return element;
-  }
-}
+//   render(): VirtualNode {
+//     console.log('render App');
+//     const element = new VirtualNode('div', '', [
+//       'hello',
+//       new VirtualNode('h1', '', ['hello']),
+//       new VirtualNode('h4', '', ['world']),
+//       new VirtualNode('div', '', [
+//         new VirtualNode('span', '', [`${this.state.count}`], {
+//           type: 'click',
+//           callback: () => {
+//             this.setState<IAppState>({
+//               ...this.state,
+//               count: this.state.count + 1,
+//             });
+//           },
+//         }),
+//         new VirtualNode('span', '', ['world']),
+//         ...this.state.cars.map((car) => new CarE({ name: car.name, color: car.color }).render()),
+//       ]),
+//     ]);
+//     if (!this.element) this.element = element;
+//     return element;
+//   }
+// }
 
 const root = document.getElementById('app');
 
--- async-race/src/scripts/App.ts
+++ async-race/src/scripts/App.ts
@@ -1,12 +1,56 @@
 import Component from '../core/component/Component';
+import { IState } from '../core/component/types';
 import VirtualNode from '../core/virtual-node/VirtualNode';
+import Header from './components/Header';
+import Garage from './views/Garage';
+import Winners from './views/Winners';
+
+type RouteType = 'garage' | 'winners';
+
+type availableRoutesType = RouteType[];
+
+interface IAppState extends IState {
+  availableRoutes: availableRoutesType;
+  activeRoute: RouteType;
+}
 
 class App extends Component {
-  //   constructor() {}
+  state: IAppState;
+
+  constructor() {
+    super();
+    this.state = {
+      availableRoutes: ['garage', 'winners'],
+      activeRoute: 'garage',
+    };
+  }
+
+  changeRoute(route: RouteType) {
+    this.setState({ ...this.state, route });
+  }
+
+  renderRoute(): VirtualNode {
+    switch (this.state.activeRoute) {
+      case 'garage': {
+        return new Garage().render();
+      }
+      case 'winners': {
+        return new Winners().render();
+      }
+      default: {
+        return new Garage().render();
+      }
+    }
+  }
 
   render() {
-    const element = new VirtualNode('div', '');
+    const element = new VirtualNode('div', '', [
+      new Header().render(),
+      new VirtualNode('main', '', [this.renderRoute()]),
+    ]);
     if (!this.element) this.element = element;
     return element;
   }
 }
+
+export default App;
