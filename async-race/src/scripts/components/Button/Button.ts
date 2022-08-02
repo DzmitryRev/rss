9a1d8002e980cb0ae32e666ee04bb942601e258f
unknown <revchenkodzmitry@gmail.com>
2022-08-02 21:31:54 +0300

    feat: finish creating styles for Button

--- async-race/src/scripts/components/Button/Button.ts
+++ async-race/src/scripts/components/Button/Button.ts
@@ -1,9 +1,19 @@
 import Component from '../../../core/component/Component';
+import VirtualNode from '../../../core/virtual-node/VirtualNode';
+import { ButtonPropsType } from './Button.types';
+import './Button.css';
 
-class Button extends Component {
-  constructor() {}
-
-  render() {}
+class Button extends Component<ButtonPropsType> {
+  render() {
+    const element = new VirtualNode('button', `button ${this.props.color}`, [this.props.title], {
+      type: 'click',
+      callback: () => {
+        this.props.event();
+      },
+    });
+    if (!this.element) this.element = element;
+    return element;
+  }
 }
 
 export default Button;
--- async-race/src/scripts/components/Header/Header.ts
+++ async-race/src/scripts/components/Header/Header.ts
@@ -1,19 +1,34 @@
 import Component from '../../../core/component/Component';
 import VirtualNode from '../../../core/virtual-node/VirtualNode';
+import Button from '../Button/Button';
 import { IHeaderProps } from './Header.types';
 
 class Header extends Component<IHeaderProps> {
   render() {
     const element = new VirtualNode('header', '', [
       'HEADER',
-      ...this.props.availableRoutes.map(
-        (route) => new VirtualNode('button', '', [route], {
-          type: 'click',
-          callback: () => {
-            this.props.changeRoute(route);
-          },
-        }),
-      ),
+      //   ...this.props.availableRoutes.map(
+      //     (route) => new VirtualNode('button', '', [route], {
+      //       type: 'click',
+      //       callback: () => {
+      //         this.props.changeRoute(route);
+      //       },
+      //     }),
+      //   ),
+      new Button({
+        title: 'Garage',
+        color: 'green',
+        event: () => {
+          this.props.changeRoute('garage');
+        },
+      }).render(),
+      new Button({
+        title: 'Winners',
+        color: 'blue',
+        event: () => {
+          this.props.changeRoute('winners');
+        },
+      }).render(),
     ]);
     if (!this.element) this.element = element;
     return element;
